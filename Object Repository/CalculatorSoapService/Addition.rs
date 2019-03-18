ee1bb024d2f0f864ec590fbf28f1ab04cabf74f9
PK19 <PK19@DESKTOP-4T9EF5T.wks.jnj.com>
2019-03-18 18:11:44 +0530

    First commit

---
+++ Object Repository/CalculatorSoapService/Addition.rs
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>Calculator Addition</description>
+   <name>Addition</name>
+   <tag></tag>
+   <elementGuidId>dbdbe3df-e678-4c0b-9b74-943055b5b176</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <restRequestMethod></restRequestMethod>
+   <restUrl></restUrl>
+   <serviceType>SOAP</serviceType>
+   <soapBody>&lt;Envelope xmlns=&quot;http://schemas.xmlsoap.org/soap/envelope/&quot;>
+    &lt;Body>
+        &lt;Add xmlns=&quot;http://tempuri.org/&quot;>
+            &lt;intA>10&lt;/intA>
+            &lt;intB>30&lt;/intB>
+        &lt;/Add>
+    &lt;/Body>
+&lt;/Envelope></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod>SOAP</soapRequestMethod>
+   <soapServiceFunction>Add</soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import org.junit.After
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyResponseStatusCode(response, 200)
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+
+
+
+
+
+
+assertThat(response.getResponseText()).contains('AddResult')
+
+
+WS.verifyElementPropertyValue(response, 'AddResponse.AddResult', '40')</verificationScript>
+   <wsdlAddress>http://www.dneonline.com/calculator.asmx?WSDL</wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>cd6c278e-a7f3-4fa2-ad4e-7ea507947548</testSuiteGuid>
+   <testCaseLink>
+      <guid>c781ccd5-9d14-4d79-b651-ec9264056f85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8ea203ee-6695-4045-963f-b5a18b1b0851</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListusersTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
